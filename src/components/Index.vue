eaf71eeb8904b4d8909f51b458b844af646f6360
chenzt <chenzt>
2021-02-19 10:50:12 +0800

    大致样子

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,33 +1,57 @@
 <template>
-  <div class="home">
-    <el-container class="el-container">
-      <el-header class="el-header">
-        <el-row :gutter="20" class="el-row row-bg">
-          <el-col :span="6" class="el-col">
-            <div class="grid-content bg-purple"><router-link to="/home">Home</router-link></div>
-          </el-col>
-          <el-col :span="6" class="el-col">
-            <div class="grid-content bg-purple"></div>
-          </el-col>
-          <el-col :span="6" class="el-col">
-            <div class="grid-content bg-purple"></div>
-          </el-col>
-          <el-col :span="6" class="el-col">
-            <div class="grid-content bg-purple"></div>
-          </el-col>
-        </el-row>
-      </el-header>
-      <el-main class="el-main"></el-main>
-    </el-container>
-  </div>
+  <el-container>
+    <el-header>
+      <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1">
+          <el-link href="/home">首页</el-link>
+        </el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">分类推荐</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
+          </el-submenu>
+        </el-submenu>
+        <el-menu-item index="3" disabled>消息中心</el-menu-item>
+        <el-menu-item index="4"><router-link :to="{name: 'Store'}">我的商铺</router-link></el-menu-item>
+      </el-menu>
+    </el-header>
+    <el-main>
+      <Home v-if="homeshow"></Home>
+
+    </el-main>
+  </el-container>
 </template>
 
 <script>
+import Home from './Home'
+
 export default {
   name: 'Index',
+  components: {Home},
   data () {
     return {
-      msg: 'Home'
+      activeIndex: '1',
+      activeIndex2: '1',
+      homeshow: true
+
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
     }
   }
 }
@@ -35,54 +59,16 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.el-header, .el-footer {
-  background-color: #B3C0D1;
-  color: #333;
-  text-align: center;
-  line-height: 60px;
-}
 .el-main {
-  width: 100%;
   background-color: #E9EEF3;
   color: #333;
   text-align: center;
   line-height: 160px;
 }
-
-body > .el-container {
-  margin-bottom: 40px;
-}
-
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
-
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
-}
-.el-row {
-  margin-bottom: 20px;
-}
-.el-col {
-  width: 300px;
-  border-radius: 4px;
-}
-.bg-purple-dark {
-  background: #99a9bf;
-}
-.bg-purple {
-  background: #d3dce6;
-}
-.bg-purple-light {
-  background: #e5e9f2;
-}
-.grid-content {
-  border-radius: 4px;
-  min-height: 36px;
-}
-.row-bg {
-  padding: 10px 0;
-  background-color: #f9fafc;
+.el-header {
+  background-color: #B3C0D1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
 }
 </style>
